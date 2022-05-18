7c678e37a3b393acf7936e51be29635ca6170457
AdarshPatil42 <adarsh.ppatil1996@gmail.com>
2022-05-18 22:46:03 +0530

    valid Token
    Signed-off-by: AdarshPatil42 <adarsh.ppatil1996@gmail.com>

--- 4.Food-delivery-App/src/middlewares/auth.js
+++ 4.Food-delivery-App/src/middlewares/auth.js
@@ -1,11 +1,23 @@
+
+const jwt = require("jsonwebtoken");
 // Take req. and verify if token is passed and valid.
 
 const authenticate = (req, res, next)=>{
     // 1. Read token
+    const token = req.headers["authorization"];
+    if(!token){
+        return res.status(401).send("Unauthorized: Token Required")
+    }
     // 2. check if token is valid
+            try{
+                const payLoad = jwt.verify(token, "THISISMYKEY");
+                req.userID = payLoad.userid;
+            }catch(err){
+                return res.status(401).send("Unauthorized: Invalid Token")
+            }
     // 3. If valid, proceed to next middleware
-    // 4. else return 401 response
-    return res.status(401).send("Unauthorized");
+    next();
+    
 }
 
 module.exports = authenticate;
