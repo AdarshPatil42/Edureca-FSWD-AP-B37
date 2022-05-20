cd731cf4466be8d3c5e3f685b39931d0afe77c67
AdarshPatil42 <adarsh.ppatil1996@gmail.com>
2022-05-20 20:35:11 +0530

    Delete
    Signed-off-by: AdarshPatil42 <adarsh.ppatil1996@gmail.com>

--- 4.Food-delivery-App/src/restaurant/models/restaurant.js
+++ 4.Food-delivery-App/src/restaurant/models/restaurant.js
@@ -1,8 +1,8 @@
 // prototype for Restaurant
 
 module.exports = class Restaurant{
-
-    constructor(_name, _location, _contact){
+    constructor(_name, _location, _contact, id){
+        this._id = id;
         this.name =_name;
         this.location =_location;
         this.contact =_contact;
--- 4.Food-delivery-App/src/restaurant/repositories/restaurant.js
+++ 4.Food-delivery-App/src/restaurant/repositories/restaurant.js
@@ -1,9 +1,37 @@
 const mongodbConfig = require("../../../config/mongodb");
+const ObjectId = require("mongodb").ObjectId;
 
 exports.add = async (restaurant)=>{
     const collection = mongodbConfig.getCollection("Restaurant");
     try{
         await collection.insertOne({name:restaurant.name, location:restaurant.location, contact:restaurant.contact});
+    }catch(err){
+        console.log(err);
+        return false;
+    }
+    return true;
+}
+
+exports.update = async (restaurant)=>{
+    const collection = mongodbConfig.getCollection("Restaurant");
+    try{
+        // object id  convert string to objectId
+        const filter = {_id:ObjectId(restaurant._id)};
+        // set operator tell mongodb to these data items
+        const update = {$set: {name: restaurant.name, location: restaurant.location, contact: restaurant.contact}};
+        await collection.findOneAndUpdate(filter, update);
+    }catch(err){
+        console.log(err);
+        return false;
+    }
+    return true;
+}
+
+exports.delete = async (id)=>{
+    const collection = mongodbConfig.getCollection("Restaurant");
+    try{
+        const filter = {_id:ObjectId(id)};
+        collection.findOneAndDelete(filter);
     }catch(err){
         console.log(err);
         return false;
