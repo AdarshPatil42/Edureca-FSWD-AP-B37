6781ce60c751395aa91762202e8fe02e57e2be62
AdarshPatil42 <adarsh.ppatil1996@gmail.com>
2022-05-22 22:53:16 +0530

    mongoose add and get
    Signed-off-by: AdarshPatil42 <adarsh.ppatil1996@gmail.com>

---
+++ 5. food-delivery-app-mongoose/src/controllers/menu.js
@@ -0,0 +1,18 @@
+
+
+const Menu = require("../models/menu");
+const repo = require("../repositories/menu");
+
+exports.add = async (req, res)=>{
+    const newMenu = new Menu(req.body);
+    const result = await repo.add(newMenu);
+    if(result){
+        res.status(200).send("Data Added");
+    }else{
+        res.status(503).send("Data Not Added");
+    }
+}
+
+exports.add = async(req, res)=>{
+
+}
